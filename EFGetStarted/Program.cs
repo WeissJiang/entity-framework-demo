9a03cb307cbe8e751de035996cd314a7b6d53376
Weiss Jiang <meweissjiang@gmail.com>
2024-02-11 12:18:03 +1100

    create BloggingContext and DbInitializer

---
+++ EFGetStarted/Program.cs
@@ -0,0 +1,53 @@
+using EFGetStarted;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var connectString = builder.Configuration.GetConnectionString("BloggingContext");
+builder.Services.AddDbContext<BloggingContext>(options =>
+  options.UseSqlServer(connectString);
+
+
+
+
+using var db = new BloggingContext();
+// Note: This sample requires the database to be created before running.
+Console.WriteLine($"Database path: {db.DbPath}.");
+
+
+
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+
+    var context = services.GetRequiredService<BloggingContext>();
+    context.Database.EnsureCreated();
+     DbInitializer.Initialize(context);
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
